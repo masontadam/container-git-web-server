038e2619ac220b807463557cda38db6daab9a3e9
Mason Adam <masontadam@gmail.com>
2018-08-22 06:23:54 -0700

    multiple scripts output on newlines

--- server_docs/server.py
+++ server_docs/server.py
@@ -6,20 +6,19 @@ import subprocess
 app = Flask(__name__)
 
 def run_scripts():
-	result = ""
+	end = []
 	for filename in os.listdir('/www/web/scripts/'):
 		out = subprocess.Popen(["/bin/sh", "-c", '/www/web/scripts/' + filename], 
            		stdout=subprocess.PIPE, 
            		stderr=subprocess.STDOUT)
 		stdout,stderr = out.communicate()
-		result += stdout
-		result += '\n'
-	return result
+		end.append(stdout)
+	return end
 
 @app.route('/')
 def index():
 	result = run_scripts()
-	return render_template('index.html', output = result)
+	return render_template('index.html', outputs = result)
 
 if __name__ == '__main__':  
      app.run(host='0.0.0.0', port=443, debug=True, ssl_context=('/www/web/ssl/server.crt', '/www/web/ssl/server.key'))
